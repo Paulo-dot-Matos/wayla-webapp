4795eb05b92acc3ef76db2a971025fc27b30647d
Paulo-dot-Matos <pauloromatos@outlook.pt>
2020-12-08 13:43:46 +0000

    Table fetching data from firebase

---
+++ wayla/models/movies.js
@@ -0,0 +1,9 @@
+class Movie{
+    constructor(owner, added, description, active, url){
+        this.owner=owner;
+        this.added=added;
+        this.description=description;
+        this.active=active;
+        this.url=url;
+    }
+}
--- wayla/routes/mainPage.js
+++ wayla/routes/mainPage.js
@@ -5,7 +5,30 @@ const createError = require('http-errors');
 const config = require('config');
 
 router.get('/', async function(req, res, next) {
-    res.render('mainPage',{ title: config.get('appName') });
+    let movies = await db.collection('movies').get();
+    let moviesArray = [];
+    for (i=0; i < movies.size; i++){
+        let movie = movies.docs[i].data() 
+        moviesArray.push( {
+            owner: movie.owner,
+            title: movie.title
+        });
+        console.log(moviesArray)
+    }
+    res.render('mainPage',{ title: config.get('appName'),movies: moviesArray});
   });
   
   module.exports = router;
+
+  class Movie{
+     
+    constructor(owner, added, description, active, url){
+        this.owner=owner;
+        this.added=added;
+        this.description=description;
+        this.active=active;
+        this.url=url;
+    }
+
+    
+}

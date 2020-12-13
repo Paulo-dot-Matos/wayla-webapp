5207ca1c9e16b090f9e5f64bb916a02b451f9b40
Paulo-dot-Matos <pauloromatos@outlook.pt>
2020-12-13 11:40:27 +0000

    list videos with map function

--- wayla/routes/mainPage.js
+++ wayla/routes/mainPage.js
@@ -7,28 +7,12 @@ const config = require('config');
 router.get('/', async function(req, res, next) {
     let movies = await db.collection('movies').get();
     let moviesArray = [];
-    for (i=0; i < movies.size; i++){
-        let movie = movies.docs[i].data() 
-        moviesArray.push( {
-            owner: movie.owner,
-            title: movie.title
-        });
-        console.log(moviesArray)
-    }
+    movies.docs.map(function(doc){
+        console.log(doc.id)
+        moviesArray.push(doc.data());
+    });
+    console.log(moviesArray);
     res.render('mainPage',{ title: config.get('appName'),movies: moviesArray});
   });
   
   module.exports = router;
-
-  class Movie{
-     
-    constructor(owner, added, description, active, url){
-        this.owner=owner;
-        this.added=added;
-        this.description=description;
-        this.active=active;
-        this.url=url;
-    }
-
-    
-}

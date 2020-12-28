33cd73d99c2502b2b488e9b4a2f767e57b2ee1a8
Paulo-dot-Matos <pauloromatos@outlook.pt>
2020-12-28 17:25:45 +0000

    Bug fix when there was no revies to shows in the videos

--- wayla/routes/video.js
+++ wayla/routes/video.js
@@ -6,16 +6,22 @@ const {db, collection, getTime} = require('../services/firebase')
 router.get('/', async function(req, res, next) {
     let movies = await db.collection('movies').doc(req.query.id).get();
     let reviews = await db.collection('movies').doc(req.query.id).collection('reviews').get();
-    let user = await db.collection(collection).doc(reviews.docs[0].data().owner).get();
+    
 
-    let map = new Map();
-    reviews.docs.map(async function(review){
-        let data = review.data()
-        data.owner = user.data().name;
-        map.set(review.id,data)
-    });
+    if (!reviews.empty) {
+      let user = await db.collection(collection).doc(reviews.docs[0].data().owner).get();
+      let map = new Map();
+      reviews.docs.map(async function(review){
+          let data = review.data()
+          data.owner = user.data().name;
+          map.set(review.id,data)
+      });
+  
+      res.render('video',{ url: movies.data().url,reviews: Object.fromEntries(map)});
+    } else {
+      res.render('video',{ url: movies.data().url});
+    }
 
-    res.render('video',{ url: movies.data().url,reviews: Object.fromEntries(map)});
   });
 
   router.get('/addVideo', function(req, res, next) {

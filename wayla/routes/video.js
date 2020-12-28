e40c35ce41d5136e3889d9e21aed7270b55d8da5
Paulo-dot-Matos <pauloromatos@outlook.pt>
2020-12-28 14:45:42 +0000

    List videos completed

--- wayla/routes/video.js
+++ wayla/routes/video.js
@@ -5,7 +5,17 @@ const {db, collection} = require('../services/firebase')
 
 router.get('/', async function(req, res, next) {
     let movies = await db.collection('movies').doc(req.query.id).get();
-    res.render('video',{ url: movies.data().url});
+    let reviews = await db.collection('movies').doc(req.query.id).collection('reviews').get();
+    let user = await db.collection(collection).doc(reviews.docs[0].data().owner).get();
+
+    let map = new Map();
+    reviews.docs.map(async function(review){
+        let data = review.data()
+        data.owner = user.data().name;
+        map.set(review.id,data)
+    });
+
+    res.render('video',{ url: movies.data().url,reviews: Object.fromEntries(map)});
   });
   
   module.exports = router;
